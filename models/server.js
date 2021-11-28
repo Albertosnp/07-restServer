8b62335629e179fb25886b300a6b674406cba693
Alberto Snp <albertosnp91@gmail.com>
2021-11-28 16:06:11 +0100

    Added class server model

---
+++ models/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
+
+    this.routes()
+  }
+  //se establecen las rutas
+  routes() {
+    app.get('/', (req, res) => {
+      res.send('Hello World')
+    })
+  }
+  //Listeners
+  listeners() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}`);
+    })
+  }
+
+}
+
+module.exports = Server;
